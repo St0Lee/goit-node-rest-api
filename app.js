f80a650e6b55d53ca522e07c7e6abb13946d0e14
St0Lee <staslee00@gmail.com>
2024-04-02 15:22:22 +0300

    Commit-02

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose"
-import dotenv from "dotenv/config"
+import "dotenv/config"
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -23,8 +23,6 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-console.log(process.env.DB_HOST)
 
 mongoose.connect(process.env.DB_HOST)
   .then(() => {
